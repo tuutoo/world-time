33308776001ef06ab41b3553d4640166a1d19f4b
tuutoo.iMac <tuutoo@iMac.com>
2022-06-21 00:15:54 +0800

    chore: setup eslint

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 const props = defineProps<{
-  name: string;
+  name: string
 }>()
 
 const upperCase = computed(() => props.name.toUpperCase())
 </script>
 
 <template>
-  <div text="red-500 3xl" font="sans bold">Hello, {{ upperCase }}</div>
-  <div i-carbon-bee text="yellow 5xl"></div>
+  <div text="red-500 3xl" font="sans bold">
+    Hello, {{ upperCase }}
+  </div>
+  <div i-carbon-bee text="yellow 5xl" />
 </template>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { fileURLToPath, URL } from 'url';
-
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import Components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import UnoCSS from 'unocss/vite';
-import { presetAttributify, presetIcons, presetUno } from 'unocss';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,4 +19,4 @@ export default defineConfig({
       presets: [presetUno(), presetAttributify(), presetIcons()],
     }),
   ],
-});
+})
