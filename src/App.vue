a75ca6f8625951abe2c33547beb50e4f7c989efc
tuutoo.iMac <tuutoo@iMac.com>
2022-06-26 01:10:11 +0800

    feat: timzone actions

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
 <template>
   <div font-sans bg-canvas h-screen>
     <NavBar />
-    <div border="~ base rounded" bg-base shadow m2>
+    <div
+      border="~ base rounded"
+      bg-base shadow mx10 mt2
+    >
       <ActionBar />
       <TimeScheduler />
     </div>
